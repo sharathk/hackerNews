882f03be58ad10fa110db182d76aebf096b7dee9
ksharath <ksharath@gmail.com>
2019-01-16 17:45:10 -0500

    fetch API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Item extends React.Component {
+  render() {
+      var post = this.props.post;
+      return <li>
+          <a href={post.url}>{post.title}</a>
+          <Footer post={post}/>
+     </li>;
+  }
+}
+
+class Footer extends React.Component {
+  render() {
+      var post = this.props.post;
+      return <small>
+          {post.points} points by {post.postedBy + ' ' + post.postedAgo} |
+          { post.commentCount } { post.commentCount === 1 ? ' comment' : ' comments' }
+      </small>;
+  }
+}
+
+class Posts extends React.Component {
+  constructor() {
+    super();
+    this.state = { posts: [] };
+  }
+  
+  componentWillMount() {
+      this.fetchLatestNews();
+  }
+  
+  fetchLatestNews() {
+    fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+      .then(response => response.json())
+      .then((data) => {
+        data.map((newsId) => {
+          fetch(` https://hacker-news.firebaseio.com/v0/item/${newsId}.json`)
+          .then(response => response.json())
+          .then((itemDetail) => {
+            console.log(`Fetched ${itemDetail.id}`)
+            this.setState((currentState) => {
+              currentState.posts.push(itemDetail);
+              return { posts: currentState.posts };
+            })
+          })
+        });
+    })
+  }
+  
+  render() {
+      return <ol className="posts">
+          {this.state.posts.map(function (post) {
+              return <Item key={post.id} post={post}/>
+          })}
+      </ol>;
+  }
+}
+
+class Header extends React.Component {
+  render() {
+      return <header>
+          <div className="container">
+              <h1 className="logo">Hacker News</h1>
+          </div>
+      </header>
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+          <Header/>
+          <div className="container content">
+              <Posts/>
+          </div>
       </div>
     );
   }
